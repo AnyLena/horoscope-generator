e088962177dafb79b1ec6758db05150841f7f252
LenaBultmann <lena.bultmann@gmx.de>
2023-12-07 13:54:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const prompt1 = [
+  "Mars is in the sign of Scorpio, ",
+  "The sun enters the house of Leo, ",
+  "Mercury is in Aquarius, ",
+  "Venus moves into Pisces, ",
+  "Saturn is in Capricorn, ",
+  "Uranus enters Taurus, ",
+  "Neptune enters Virgo, ",
+  "Pluto moves through Sagittarius, ",
+  "Jupiter is in Aquarius, ",
+  "Mercury enters the sign of Cancer, ",
+];
+const prompt2 = [
+  "influencing your emotional stability. ",
+  " bringing success in your career. ",
+  "leading to clear communications. ",
+  "positively impacting your love life. ",
+  "and urges you to hold onto your long-term goals. ",
+  "and brings changes to your financial area. ",
+  "and fosters your spiritual development. ",
+  "which can lead to profound transformations. ",
+  "encouraging you to explore new ideas. ",
+  "promoting emotional openness. ",
+];
+const prompt3 = [
+  "A new opportunity is on the horizon for you",
+  "Your creativity will soar to new heights this month",
+  "Financial abundance is coming your way",
+  "An unexpected encounter could lead to a significant relationship change",
+  "Stay open to new connections, as the universe has exciting plans for you",
+  "Travel plans are in your future, and they will bring positive changes",
+  "Your career is about to take off in a big way",
+  "A major breakthrough in personal growth awaits you this year",
+  "Love and romance are in the stars for you right now",
+  "You will find success by following your intuition and taking risks",
+];

1296ff57606ceb601926d86d46962763cd058557
LenaBultmann <lena.bultmann@gmx.de>
2023-12-07 15:15:53 +0100

    adding some styling to dropdown

--- app.js
+++ app.js
@@ -35,56 +35,54 @@ const prompt3 = [
   "You will find success by following your intuition and taking risks.",
 ];
 
-
-const horoscopeDiv = document.getElementById('horoscope-text')
-const horoscopeBtn = document.getElementById('start-btn')
-const dropdown = document.getElementById('myDropdown')
-const dropdownClick = document.getElementById('dropdown-click')
-const starSigns = document.querySelectorAll('.sign')
+const horoscopeDiv = document.getElementById("horoscope-text");
+const horoscopeBtn = document.getElementById("start-btn");
+const dropdown = document.getElementById("myDropdown");
+const dropdownClick = document.getElementById("dropdown-click");
+const starSigns = document.querySelectorAll(".sign");
 
 // Creating Random Horoscope Text
 
 const handleText = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const randomNum1 = Math.floor(Math.random() * prompt1.length)
-    const randomNum2 = Math.floor(Math.random() * prompt2.length)
-    const randomNum3 = Math.floor(Math.random() * prompt3.length)
-    
-    const horoscope = `${prompt1[randomNum1]} ${prompt2[randomNum2]} ${prompt3[randomNum3]}`
-    
-    horoscopeDiv.innerHTML = horoscope
-}
+  const randomNum1 = Math.floor(Math.random() * prompt1.length);
+  const randomNum2 = Math.floor(Math.random() * prompt2.length);
+  const randomNum3 = Math.floor(Math.random() * prompt3.length);
+
+  const horoscope = `${prompt1[randomNum1]} ${prompt2[randomNum2]} ${prompt3[randomNum3]}`;
+
+  horoscopeDiv.innerHTML = horoscope;
+};
 
 horoscopeBtn.addEventListener("click", handleText);
 
 // Creating Dropdown With Star Signs That Has no Other Use Than Looking Authentic
 
 const handleDropdown = () => {
-    dropdown.classList.add('show') 
-}
+  dropdown.classList.add("show");
+};
 
-dropdownClick.addEventListener('click', handleDropdown);
+dropdownClick.addEventListener("click", handleDropdown);
 
 const handleStar = (e) => {
-    const text = e.target.id
-    const capitalized = text.charAt(0).toUpperCase() + text.slice(1);
+  const text = e.target.id;
+  const capitalized = text.charAt(0).toUpperCase() + text.slice(1);
 
-    dropdownClick.innerHTML = capitalized;
+  dropdownClick.innerHTML = capitalized;
 
-    dropdown.classList.remove('show');
-}
+  dropdown.classList.remove("show");
+};
 
-starSigns.forEach( element => element.addEventListener('click', handleStar))
-
-
+starSigns.forEach((element) => element.addEventListener("click", handleStar));
 
 // Adding Stars for Ambience
 
-for (let i = 0; i < 100; i++) { // create 100 stars
-    const star = document.createElement('div');
-    star.className = 'star';
-    star.style.left = Math.random() * window.innerWidth + 'px'; 
-    star.style.top = Math.random() * window.innerHeight + 'px'; 
-    document.body.appendChild(star);
+for (let i = 0; i < 100; i++) {
+  // create 100 stars
+  const star = document.createElement("div");
+  star.className = "star";
+  star.style.left = Math.random() * window.innerWidth + "px";
+  star.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(star);
 }
